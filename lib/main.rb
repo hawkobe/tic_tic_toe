dca5957c3a060f4a2594ffbdd02431a56bacacf2
Jacob Sanders <sanders.jms@gmail.com>
2022-06-23 16:09:57 -0600

    Changed require

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,5 @@
-require '.lib/tic_tac_toe.rb'
-require '.lib/player.rb'
-require '.lib/board.rb'
+require 'lib/tic_tac_toe.rb'
+require 'lib/player.rb'
+require 'lib/board.rb'
 
 TicTacToe.new.start_game
