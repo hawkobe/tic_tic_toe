44a6162c91e4ca65dab1563effe1f977075da099
Jacob Sanders <sanders.jms@gmail.com>
2022-06-23 16:23:12 -0600

    still trying to figure out require

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,5 @@
 require_relative './tic_tic_toe.rb'
-require_relative './player.rb'
-require_relative './board.rb'
+# require_relative './player.rb'
+# require_relative './board.rb'
 
 TicTacToe.new.start_game
